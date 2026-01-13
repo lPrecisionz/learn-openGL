36e225ad214b6b715b6dc7ece72d2ffb1730131d
lPrecisionz <pedrodocarmo1@gmail.com>
2026-01-13 19:31:17 -0300

    implement shader compilation

--- 03_triangleRemake/include/renderer.hpp
+++ 03_triangleRemake/include/renderer.hpp
@@ -6,22 +6,35 @@ namespace Prec{
 
 constexpr unsigned int LOCAL_VERSION = 3;
 
+enum SHADER_KIND{
+  VERTEX   = GL_VERTEX_SHADER,
+  FRAGMENT = GL_FRAGMENT_SHADER
+};
+
 class Renderer{
 public:
-  GLFWwindow *m_window;
-  const char *m_window_name;
+  const char  *m_window_name;
+  GLFWwindow  *m_window;
+  unsigned int m_vertex_shader;
+  unsigned int m_fragment_shader;
+  unsigned int m_shader_program;
 
 public:
   Renderer() : m_window(nullptr) {};
   ~Renderer();
   void init(const char* window_name);
   void process_input();
-
+  void init_shader(const SHADER_KIND kind, const char* src_path);
+  void init_fragment_shader(const char *src_path);
+  void link_program();
+  
 private: 
   const unsigned int base_width =  800; 
   const unsigned int base_height = 600;
+  void delete_shaders();
+
 };
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
-} //namespace Prec
+} //namespace Precsr
--- 03_triangleRemake/src/renderer.cpp
+++ 03_triangleRemake/src/renderer.cpp
@@ -34,6 +34,55 @@ void Renderer::process_input(){
   }
 }
 
+void Renderer::init_shader(const SHADER_KIND kind, const char* src_path){
+  unsigned int *curr_shader = nullptr;
+  unsigned int src_string_count = 1;
+
+  if(kind == GL_VERTEX_SHADER)
+    curr_shader = &m_vertex_shader;
+  
+  if(kind == GL_FRAGMENT_SHADER)
+    curr_shader = &m_fragment_shader;
+
+  m_vertex_shader = glCreateShader(kind);
+  glShaderSource(*curr_shader, src_string_count, &src_path, NULL);
+  glCompileShader(*curr_shader);
+  
+  int success = 0;
+  char info_log[512];
+  glGetShaderiv(*curr_shader, GL_COMPILE_STATUS, &success);
+  if(!success){
+    glGetShaderInfoLog(*curr_shader, 512, NULL, info_log);
+    std::cerr << "ERROR::SHADER::COMPILATION" << std::endl << info_log;
+  }
+
+  std::cout << "Compiled shader -\t" << *curr_shader << std::endl;
+}
+
+void Renderer::link_program(){
+  m_shader_program = glCreateProgram();
+  glAttachShader(m_shader_program, m_vertex_shader);
+  glAttachShader(m_shader_program, m_fragment_shader);
+  glLinkProgram(m_shader_program);
+  
+  int success = 0;
+  char info_log[512];
+  glGetProgramiv(m_shader_program, GL_LINK_STATUS, &success);  
+  if(!success){
+    glGetProgramInfoLog(m_shader_program, 512, NULL, info_log);
+    std::cout << "ERROR::SHADER::PROGRAM" << std::endl;
+  }
+
+  delete_shaders();
+  std::cout << "Compiled Program -\t" << m_shader_program << std::endl;
+}
+
+void Renderer::delete_shaders(){
+  glDeleteShader(m_vertex_shader);
+  glDeleteShader(m_fragment_shader);
+  std::cout << "Deleted Shaders -\t" << "pedrin" << std::endl;
+}
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height){
   glViewport(0,0,width,height);
 }
