7a258235348f39214093245341c54d142a6c5822
lPrecisionz <pedrodocarmo1@gmail.com>
2026-01-14 18:00:24 -0300

    exercise 3

--- 04_shaders/shaders/vertex.glsl
+++ 04_shaders/shaders/vertex.glsl
@@ -2,10 +2,13 @@
 layout (location=0) in vec3 aPos;
 layout (location=1) in vec3 aColor;
 
-uniform float h_offset;
+uniform float h_offset = 0;
 out vec3 our_color;
+out vec3 out_pos;
+
 void main()
 {
   our_color = aColor;
-  gl_Position = vec4(aPos.x + h_offset, -aPos.y, aPos.z, 1.0);
+  gl_Position = vec4(aPos.x + h_offset, aPos.y, aPos.z, 1.0);
+  out_pos = aPos;
 };
